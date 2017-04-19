b237d978490e4e6329e7dc0feb5180661e10d85d
JK-Flip-Flop96 <stuye98@gmail.com>
2017-04-19 16:21:31 +0100

    added "s"

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function test(){
 
 	console.log("button pressed");
 
-	$.getJSON("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=784611CFD37EB6A654286A2536412E62&steamid=76561198078101135", function(result){
+	$.getJSON("https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=784611CFD37EB6A654286A2536412E62&steamid=76561198078101135", function(result){
 
 		console.log("json return");
 	});
