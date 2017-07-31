76c4f5fd1ceb0408e9a8e5d240d2798c2880e074
JK-Flip-Flop96 <stuye98@gmail.com>
2017-07-31 22:17:20 +0100

    Source checkboxes filtering and refreshing

--- js/main.js
+++ js/main.js
@@ -2,82 +2,108 @@ console.log('JS Started');
 
 let getSources = function (state) {
     $.getJSON("https://newsapi.org/v1/sources?language=en&category=technology", function (json) {
-        console.log(json)
 
         let sourcesList = [];
         let sourceNames = [];
         let sourceSorts = [];
+        let sourceURLs = [];
         let sourceEnabled = [];
 
         let reset = "";
         $('#articles').html(reset);
 
         for (let i = 0; i < json.sources.length; i++) {
-           sourcesList[i] = json.sources[i].id;
-           sourceNames[i] = json.sources[i].name;
-           sourceSorts[i] = json.sources[i].sortBysAvailable;
-           if($('input[name=source]').val()){
-               sourceEnabled[i] = true;
-           }else{
-               sourceEnabled[i] = false;
-           }
+            sourcesList[i] = json.sources[i].id;
+            sourceNames[i] = json.sources[i].name;
+            sourceSorts[i] = json.sources[i].sortBysAvailable;
+            sourceURLs[i] = json.sources[i].url;
+            sourceEnabled[i] = $('input[value=' + sourcesList[i] + ']').is(':checked');
         }
-
+        console.log(sourceEnabled);
 
 
         console.log(sourcesList);
 
         let sort;
-        if(state === 'Top'){
+        if (state === 'Top') {
             sort = 'top'
-        }else{
+        } else {
             sort = 'latest'
         }
 
-        for(let i = 0; i < sourcesList.length; i++) {
+        for (let i = 0; i < sourcesList.length; i++) {
 
-        	if(sourcesList[i] !== "hacker-news") {
+            let load = true;
+
+            if (sourcesList[i] === "hacker-news") {
+                load = false
+            }
+
+            if ($.inArray('latest', sourceSorts[i]) === -1 && sort === 'latest') {
+                load = false
+            }
+
+            if ($.inArray('top', sourceSorts[i]) === -1 && sort === 'top') {
+                load = false
+            }
+
+            if (!sourceEnabled[i]) {
+                load = false
+            }
+
+            console.log(load + " " + sourceNames[i]);
+            if (load === true) {
                 $.getJSON("https://newsapi.org/v1/articles?source=" + sourcesList[i] + "&sortBy=" + sort + "&apiKey=3b3c91de05994045b2b4ab15df772f7e", function (json) {
-                    console.log(json);
-                    if((($.inArray('latest',sourceSorts[i]) !== -1 && sort === 'latest') || sort === 'top') && sourceEnabled[i]) {
-                        var output = '<div id="source-divider">' + sourceNames[i] + '</div>';
-                        for (let i = 0; i < (json.articles).length; i++) {
-                            console.log(i);
-                            console.log(json.articles[i].urlToImage);
-                            output += '<a href="' + json.articles[i].url + '">';
-                            output += '<div id="article">';
-                            output += '<img id = "article-photo" src=' + json.articles[i].urlToImage + '>';
-                            output += '<p id="article-headline">' + json.articles[i].title + '</p>';
-                            output += '</div></a>';
-                        }
+
+                    var output = '<a href="' + sourceURLs[i] + '" <div id="source-divider">' + sourceNames[i] + '</div></a>';
+                    for (let i = 0; i < (json.articles).length; i++) {
+                        output += '<a href="' + json.articles[i].url + '">';
+                        output += '<div id="article">';
+                        output += '<img id = "article-photo" src=' + json.articles[i].urlToImage + '>';
+                        output += '<p id="article-headline">' + json.articles[i].title + '</p>';
+                        output += '</div></a>';
                     }
+
                     $('#articles').append(output)
                 });
+            }
+
+            if($('articles').html() === ""){
+                $('status').html('<p>No Articles</p>')
+            }else{
+                $('status').html("")
             }
         }
     });
 };
 
 $(function () {
-    getSources($('input[name=sort]:selected').val());
+    $('input[value=Top]').prop('checked', true);
+
 
     $.getJSON("https://newsapi.org/v1/sources?language=en&category=technology", function (json) {
         let sourcesList = [];
         for (let i = 0; i < json.sources.length; i++) {
             sourcesList[i] = json.sources[i].id;
             if (sourcesList[i] !== "hacker-news") {
-                $('#source-list').append('<input type="checkbox" name="source" value="Top" checked>' + json.sources[i].name + '<br>')
+                $('#source-list').append('<input type="checkbox" name="source" value="' + json.sources[i].id + '" checked>' + json.sources[i].name + '<br>')
             }
         }
+
+        getSources($('input[name=sort]:checked').val());
+
+        $('input[type=checkbox]').change(function () {
+            getSources($('input[name=sort]:checked').val());
+        });
     });
+
+
 
     $('input[name=sort]').change(function () {
         getSources(this.value);
     });
 
-    //$('input[name=source]').change(function () {
-        //getSources($('input[name=sort]:selected').val());
-    //});
+
 
 });
 
