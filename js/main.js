07ecdf0d2060e47056a9aba9002a78ddd58ec021
JK-Flip-Flop96 <stuye98@gmail.com>
2017-07-31 15:31:26 +0100

    Filter sidebar mockup and more sources

--- js/main.js
+++ js/main.js
@@ -1,26 +1,54 @@
 console.log('JS Started');
 
-var getData = function(){
-	$.getJSON("https://newsapi.org/v1/articles?source=the-verge&sortBy=top&apiKey=3b3c91de05994045b2b4ab15df772f7e", function(json){
-		console.log(json);
-		$('#status').html('<h2>The Verge Status: ' + (json.status).toUpperCase() + '</h2>');
+let getSources = function () {
+    $.getJSON("https://newsapi.org/v1/sources?language=en&category=technology", function (json) {
+        console.log(json)
 
-		var output = "";
-		for(var i = 0; i < (json.articles).length; i++){
-			console.log(i);
-			console.log(json.articles[i].urlToImage)
-			output += '<p id="article">'
-            output += '<a href="' + json.articles[i].url + '">';
-            output += '<img id = "article-photo" src=' + json.articles[i].urlToImage + '>'
-            output += '<br>'
-            output += json.articles[i].title
-            output += '</p>'
-		}
+        let sourcesList = [];
 
-		$('#articles').html('<p>' + output + '</p>')
+        for (let i = 0; i < json.sources.length; i++) {
+           sourcesList[i] = json.sources[i].id;
+           if(sourcesList[i] !== "hacker-news"){
+			   $('#source-list').append('<input type="checkbox" name="source" value="Top" checked>' + json.sources[i].name + '<br>')
+           }
+        }
+
+        console.log(sourcesList);
+
+        var output = "";
+
+        for(let i = 0; i < sourcesList.length; i++) {
+
+        	if(sourcesList[i] !== "hacker-news") {
+                $.getJSON("https://newsapi.org/v1/articles?source=" + sourcesList[i] + "&sortBy=top&apiKey=3b3c91de05994045b2b4ab15df772f7e", function (json) {
+                    console.log(json);
+
+                    for (let i = 0; i < (json.articles).length; i++) {
+                        console.log(i);
+                        console.log(json.articles[i].urlToImage);
+                        output += '<div id="article">';
+                        output += '<a href="' + json.articles[i].url + '">';
+                        output += '<img id = "article-photo" src=' + json.articles[i].urlToImage + '>'
+                        output += '<p id="article-headline">' + json.articles[i].title + '</p>';
+                        output += '</div>';
+                    }
+
+                    $('#articles').append(output)
 
 
-	});
+                });
+            }
+        }
+
+    });
+};
+
+let updateSources = function () {
+
 }
 
-getData()
+
+getSources();
+
+$('#source-buttons').click(updateSources)
+
