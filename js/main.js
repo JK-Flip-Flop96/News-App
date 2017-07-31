abaa0c974bec29f03d2321fb62a2d50d2e4e1593
JK-Flip-Flop96 <stuye98@gmail.com>
2017-07-31 18:28:47 +0100

    Sort Buttons Working

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 console.log('JS Started');
 
-let getSources = function () {
+let getSources = function (state) {
     $.getJSON("https://newsapi.org/v1/sources?language=en&category=technology", function (json) {
         console.log(json)
 
         let sourcesList = [];
         let sourceNames = [];
+        let sourceSorts = [];
+
+        let reset = "";
+        $('#source-list').html(reset);
+        $('#articles').html(reset);
 
         for (let i = 0; i < json.sources.length; i++) {
            sourcesList[i] = json.sources[i].id;
            sourceNames[i] = json.sources[i].name;
+           sourceSorts[i] = json.sources[i].sortBysAvailable;
            if(sourcesList[i] !== "hacker-news"){
 			   $('#source-list').append('<input type="checkbox" name="source" value="Top" checked>' + json.sources[i].name + '<br>')
            }
@@ -17,22 +23,29 @@ let getSources = function () {
 
         console.log(sourcesList);
 
-        let reset = "";
-        $('#articles').html(reset);
+        let sort;
+        if(state === 'Top'){
+            sort = 'top'
+        }else{
+            sort = 'latest'
+        }
+
         for(let i = 0; i < sourcesList.length; i++) {
 
         	if(sourcesList[i] !== "hacker-news") {
-                $.getJSON("https://newsapi.org/v1/articles?source=" + sourcesList[i] + "&sortBy=top&apiKey=3b3c91de05994045b2b4ab15df772f7e", function (json) {
+                $.getJSON("https://newsapi.org/v1/articles?source=" + sourcesList[i] + "&sortBy=" + sort + "&apiKey=3b3c91de05994045b2b4ab15df772f7e", function (json) {
                     console.log(json);
-                    let output = '<div id="source-divider">' + sourceNames[i] +'</div>';
-                    for (let i = 0; i < (json.articles).length; i++) {
-                        console.log(i);
-                        console.log(json.articles[i].urlToImage);
-                        output += '<div id="article">';
-                        output += '<a href="' + json.articles[i].url + '">';
-                        output += '<img id = "article-photo" src=' + json.articles[i].urlToImage + '>'
-                        output += '<p id="article-headline">' + json.articles[i].title + '</p>';
-                        output += '</div>';
+                    if(($.inArray('latest',sourceSorts[i]) !== -1 && sort === 'latest') || sort === 'top') {
+                        var output = '<div id="source-divider">' + sourceNames[i] + '</div>';
+                        for (let i = 0; i < (json.articles).length; i++) {
+                            console.log(i);
+                            console.log(json.articles[i].urlToImage);
+                            output += '<a href="' + json.articles[i].url + '">';
+                            output += '<div id="article">';
+                            output += '<img id = "article-photo" src=' + json.articles[i].urlToImage + '>';
+                            output += '<p id="article-headline">' + json.articles[i].title + '</p>';
+                            output += '</div></a>';
+                        }
                     }
                     $('#articles').append(output)
                 });
@@ -41,14 +54,16 @@ let getSources = function () {
     });
 };
 
-let updateSources = function () {
+let Top = "Top";
 
-};
+$(function () {
+    getSources($('input[name=sort]:selected').val());
 
+    $('input[name=sort]').change(function () {
+        getSources(this.value);
+    });
 
-getSources();
-
-$('#source-buttons').click(updateSources);
+});
 
 
 
